08c5f1619173462b127646d0047e75fc8293f56b
joaquinsch <joacohao1@gmail.com>
2025-12-10 18:29:28 -0300

    checkeo linter

--- src/main/java/com/agenciaturismo/agenciaturismo/AgenciaturismoApplication.java
+++ src/main/java/com/agenciaturismo/agenciaturismo/AgenciaturismoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgenciaturismoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AgenciaturismoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AgenciaturismoApplication.class, args);
+    }
 }
