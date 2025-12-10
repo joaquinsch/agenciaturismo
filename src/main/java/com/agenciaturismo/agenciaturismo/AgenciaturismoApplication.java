91f271ab97eecab23c4b313f0e2777257805f543
joaquinsch <joacohao1@gmail.com>
2025-12-10 16:43:31 -0300

    primer commit

---
+++ src/main/java/com/agenciaturismo/agenciaturismo/AgenciaturismoApplication.java
@@ -0,0 +1,13 @@
+package com.agenciaturismo.agenciaturismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaturismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaturismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenciaturismo/agenciaturismo/AgenciaturismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenciaturismo.agenciaturismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaturismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
